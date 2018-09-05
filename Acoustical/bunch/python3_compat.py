ccdf37ffdcd7dd9385b911b39d0ef4482ee0f7ca
Bauernteams <broll.niklas@gmail.com>
2018-09-05 17:58:17 +0200

    changed to bunch

--- Acoustical/bunch/python3_compat.py
+++ Acoustical/bunch/python3_compat.py
@@ -5,7 +5,7 @@ _IS_PYTHON_3 = (platform.version() >= '3')
 identity = lambda x : x
 
 # u('string') replaces the forwards-incompatible u'string'
-if _IS_PYTHON_3:
+if True:
     u = identity
 else:
     import codecs
@@ -13,7 +13,7 @@ else:
         return codecs.unicode_escape_decode(string)[0]
 
 # dict.iteritems(), dict.iterkeys() is also incompatible
-if _IS_PYTHON_3:
+if True:
     iteritems = dict.items
     iterkeys  = dict.keys
 else:
